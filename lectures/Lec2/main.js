f18c8fb05c4badcd61415df81288ae91e426aa3f
Lasha Abramishvili <abramishvililasha25@gmail.com>
2026-05-06 20:52:18 +0400

    Lec2

---
+++ lectures/Lec2/main.js
@@ -0,0 +1,38 @@
+// 1
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+// 2
+// function Getcount(word, letter) {
+//   let count = 0;
+//   for (let i = 0; i <= word.length; i++) {
+//     if (word.toLowerCase()[i] === letter.toLowerCase()) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+// console.log(Getcount("JAVAscript", "A"));
+// 3
+// function longestWord(sentence) {
+//   const words = sentence.trim().split(" ");
+
+//   let longestOne = words[0];
+
+//   for (let i of words) {
+//     if (i.length > longestOne.length) {
+//       longestOne = i;
+//     }
+//   }
+
+//   return longestOne;
+// }
+// console.log(longestWord("Hello World adsifasfasiu"));
