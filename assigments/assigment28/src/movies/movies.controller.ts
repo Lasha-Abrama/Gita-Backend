eccc85268d0eaeff4ca6d853c7825097f5982cb3
Lasha Abramishvili <abramishvililasha25@gmail.com>
2026-09-06 20:26:07 +0400

    feat: add movies and directors API with filters and pagination

---
+++ assigments/assigment28/src/movies/movies.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { QueryMoviesDto } from './dto/query-movies.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Post()
+  create(@Body() createMovieDto: CreateMovieDto) {
+    return this.moviesService.create(createMovieDto);
+  }
+
+  @Get()
+  findAll(@Query() query: QueryMoviesDto) {
+    return this.moviesService.findAll(query);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.moviesService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateMovieDto: UpdateMovieDto) {
+    return this.moviesService.update(id, updateMovieDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.moviesService.remove(id);
+  }
+}
